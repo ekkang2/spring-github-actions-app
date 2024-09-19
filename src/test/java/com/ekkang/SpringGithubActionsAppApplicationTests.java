67ff6e519ad9a81eb5ff565ea2f6aabda82bb739
ekkang2 <rkddmsrud27@gmail.com>
2024-09-19 15:27:55 +0900

    github actions test1

---
+++ src/test/java/com/ekkang/SpringGithubActionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekkang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
